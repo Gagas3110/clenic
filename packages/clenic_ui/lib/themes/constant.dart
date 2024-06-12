58d09ce546623247dad0de336554a203e95d037b
Gagas Wijaksana Nugraha (ID) <G.NUGRAHA@HOMECREDIT.CO.ID>
2024-06-12 11:19:57 +0700

    bugFix : gradle (when adding firebase) feat : Color Constant

---
+++ packages/clenic_ui/lib/themes/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+const clenicPrimary = Color(0xFF2D84FB);
+const clenicGrey = Color(0xFF8C8C8C);
+const clenicAlert = Color(0xFFF71010);
+const clenicConfirm = Color(0xFF44BED0);
+const clenicWarning = Color(0xFFF9A825);
+const clenicSilverSand = Color(0xFFFFFFFF);
